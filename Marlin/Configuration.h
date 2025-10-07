cefbdfaf53e5f169a8406d7857a7619e958d08f7
Felipe-Guerche <felipe.guerche@estudante.ufscar.br>
2025-10-07 02:40:36 -0300

    feat(config): set axis steps per unit based on legacy repository
    - Changed DEFAULT_AXIS_STEPS_PER_UNIT from { 80, 80, 400, 500 } to { 160.0, 160.0, 800.0, 197.48 }
    - Based on analysis of F4xx-SIM240x320 repository Configuration.h line 1181
    - Confirmed for PROJ_CRUX1 project configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1300,7 +1300,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160.0, 160.0, 800.0, 197.48 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
